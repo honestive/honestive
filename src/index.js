68acc81028fcdb4da4da5d46b73d88084e314076
Domas <domas.bitvinskas@me.com>
2018-08-10 18:51:03 +0300

    Initial seed

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+
+const resolvers = {
+  Query: {
+    releases(parent, args, ctx, info) {
+      return ctx.db.query.releases({}, info)
+    },
+  },
+  Mutation: {
+    createRelease(parent, { version, title, description }, ctx, info) {
+      return ctx.db.mutation.createRelease(
+        {
+          data: {
+            version,
+            title,
+            description,
+          },
+        },
+        info,
+      )
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
+      endpoint: 'https://eu1.prisma.sh/public-wavepiper-993/honestive/dev', // the endpoint of the Prisma API
+      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
