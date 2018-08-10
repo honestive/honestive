282f1e88d906c1b8fc7cecb989b95118c0b467c3
Domas <domas.bitvinskas@me.com>
2018-08-11 01:55:56 +0300

    Sync releases from GitHub repository to GitHubRelease

--- src/index.js
+++ src/index.js
@@ -1,30 +1,68 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Github } = require('graphql-binding-github')
 const { Prisma } = require('prisma-binding')
+const { importSchema } = require('graphql-import')
+const _ = require('lodash')
+
+const typeDefs = importSchema('./src/schema.graphql')
+const token = 'SECRET-TOKEN'
+const gitHub = new Github(token)
+const repositoryReleasesFragment = `
+  {
+    releases(last: 100) {
+      nodes {
+        id
+        name
+        description
+        url
+        publishedAt
+        tag {
+          name
+        }
+      }
+    }
+  }
+`
 
 const resolvers = {
   Query: {
     releases(parent, args, ctx, info) {
       return ctx.db.query.releases({}, info)
     },
-  },
-  Mutation: {
-    createRelease(parent, { version, title, description }, ctx, info) {
-      return ctx.db.mutation.createRelease(
-        {
-          data: {
-            version,
-            title,
-            description,
-          },
-        },
-        info,
-      )
+    gitHubReleases(parent, args, ctx, info) {
+      return ctx.db.query.gitHubReleases(args, info)
     },
   },
+  Mutation: {
+    async syncGitHubReleases(parent, args, ctx, info) {
+      const owner = 'Nedomas'
+      const name = 'honestive-testing'
+      const repository = await gitHub.query.repository({ owner, name }, repositoryReleasesFragment)
+
+      const serialize = (release) => {
+        const result = _.omit(_.cloneDeep(release), 'id', 'tag')
+        result.gitHubId = release.id
+        result.tagName = release.tag.name
+        return result
+      }
+
+      await Promise.all(_.map(_.compact(repository.releases.nodes), (release) => {
+        const serializedRelease = serialize(release)
+        return ctx.db.mutation.upsertGitHubRelease({
+          where: _.pick(serializedRelease, 'gitHubId'),
+          create: serializedRelease,
+          update: serializedRelease,
+        })
+      }))
+
+      return true
+    },
+  },
+  ...gitHub.getAbstractResolvers(typeDefs),
 }
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs,
   resolvers,
   context: req => ({
     ...req,
